0475227ea24456a9ae74749feb35be6eceb0fe63
Min Khant Nyi Thway <domfox848@gmail.com>
2023-12-24 21:54:40 +0630

    ei amung learn done

--- tall/routes/web.php
+++ tall/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\Users\UserAdd;
+use App\Livewire\Users\UserDetail;
+use App\Livewire\Users\UserList;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
+
+
+
+Route::get('/users', UserList::class);
+Route::get('/users/view/{id}', UserDetail::class);
+Route::get('/users/add', UserAdd::class);
